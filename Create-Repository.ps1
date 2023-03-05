9cadfad530658aaed014fd6a98a26df01508aa9d
smaicas <samuel.maicas.development@gmail.com>
2023-03-05 21:13:38 +0100

    .

--- Create-Repository.ps1
+++
@@ -1,9 +0,0 @@
-$url = $args[0]
-if ($null -eq $url) {throw "Repository URL is required"}
-
-git init
-git add .
-git commit -m "init"
-git branch -M master
-git remote add origin $url
-git push -u origin master
